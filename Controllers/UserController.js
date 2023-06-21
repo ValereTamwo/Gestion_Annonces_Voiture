7d104478c85bb284201534359eab0b38e65b5cab
folong201 <folong201@gmail.com>
2023-06-21 01:06:48 +0100

    initialisation of the backend

---
+++ Controllers/UserController.js
@@ -0,0 +1,7 @@
+exports.index = (req,res)=>{
+    res.render("pages/index")
+}
+
+exports.home = (req,res)=>{
+    res.render("pages/home")
+}
---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const router = require("./Routes/Router")
+const swagger = require("./swager")
+const app = express()
+
+
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use("/assets", express.static("public"));
+
+
+
+app.use('/api-docs',swagger.serve,swagger.set);
+
+
+app.use(bodyParser.json());
+app.use(router)
+
+
+
+module.exports = app
